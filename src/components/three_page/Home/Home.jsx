cd55424a07d2aa2943a3ea974ec9002c65e68c27
Prince Antony <148847362+princeantonyno1@users.noreply.github.com>
2025-08-01 23:30:53 +0530

    Add files via upload

--- src/components/three_page/Home/Home.jsx
+++ src/components/three_page/Home/Home.jsx
@@ -11,8 +11,13 @@ import phNo from "../../../assets/reg/phno.png";
 import facial from "../../../assets/reg/facial.png";
 import personal from "../../../assets/reg/personal.png";
 import quest from "../../../assets/reg/question.png";
+
 import qr from "../../../assets/signin/scanqr.png";
 import signin from "../../../assets/signin/signin.png";
+
+import androidIcon from "../../../assets/android.png";
+import appleIcon from "../../../assets/apple.png";
+
 
 const TABS = [
   { id: "register", label: "Registration", icon: regIcon },
@@ -42,163 +47,241 @@ const ThreePage = () => {
       case "register":
         return (
           <main className={styles.main}>
-          <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
+            <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
 
-          {/* Registration process */}
-          <div className={styles.flexRow}>
-            <div>
-              <h2>Registration Process</h2>
-              <p>4 step process, it will take 2 mins of time</p>
-              <a href="#">https://fiacon.com/registraion</a>
+            {/* Registration process */}
+            <div className={styles.flexRow}>
+              <div>
+                <h2>Registration Process</h2>
+                <p>4 step process, it will take 2 mins of time</p>
+                <a href="#">https://fiacon.com/registraion</a>
+              </div>
+              <img src={cleanerIcon} />
             </div>
-            <img src={cleanerIcon} />
-          </div>
 
-          {/* Phone number */}
-          <div className={styles.flexRowSS}>
-            <img src={phNo} />
-            <div>
-              <h2>Phone Number</h2>
-              <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+            {/* Phone number */}
+            <div className={styles.flexRowSS}>
+              <img src={phNo} />
+              <div>
+                <h2>Phone Number</h2>
+                <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Personal Details */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Personal Details</h2>
-              <p>Fill in your First Name, Last Name, and select your Gender from the dropdown; all fields are mandatory, and only alphabetic characters are allowed in name fields.</p>
+            {/* Personal Details */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Personal Details</h2>
+                <p>Fill in your First Name, Last Name, and select your Gender from the dropdown; all fields are mandatory, and only alphabetic characters are allowed in name fields.</p>
+              </div>
+              <img src={personal} />
             </div>
-            <img src={personal} />
-          </div>
 
-          {/* Facial Recognition */}
-          <div className={styles.flexRowSS}>
-            <img src={facial} />
-            <div>
-              <h2>Facial Recognition</h2>
-              <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+            {/* Facial Recognition */}
+            <div className={styles.flexRowSS}>
+              <img src={facial} />
+              <div>
+                <h2>Facial Recognition</h2>
+                <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Questionnaires */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Questionaires</h2>
-              <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+            {/* Questionnaires */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Questionnaires</h2>
+                <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+              </div>
+              <img src={quest} />
             </div>
-            <img src={quest} />
-          </div>
 
-        </main>
+            <section className={styles.platforms}>
+              {/* IOS */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={appleIcon} alt="iOS" className={styles.platformIcon} />
+                  <div className={styles.flexbox}>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+
+              {/* Android */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={androidIcon} alt="Android" className={styles.platformIcon} />
+                  <div>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+            </section>
+
+          </main>
 
 
         );
       case "signin":
         return (
           <main className={styles.main}>
-          <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
+            <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
 
-          {/* Sign In process */}
-          <div className={styles.flexRow}>
-            <div>
-              <h2>Sign In Process</h2>
-              <p>4 step process, it will take 2 mins of time</p>
-              <a href="#">https://fiacon.com/signin</a>
+            {/* Sign In process */}
+            <div className={styles.flexRow}>
+              <div>
+                <h2>Sign In Process</h2>
+                <p>4 step process, it will take 2 mins of time</p>
+                <a href="#">https://fiacon.com/signin</a>
+              </div>
+              <img src={cleanerIcon} />
             </div>
-            <img src={cleanerIcon} />
-          </div>
 
-          {/* Scan QR Code */}
-          <div className={styles.flexRowSS}>
-            <img src={qr} />
-            <div>
-              <h2>Scan QR Code</h2>
-              <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+            {/* Scan QR Code */}
+            <div className={styles.flexRowSS}>
+              <img src={qr} />
+              <div>
+                <h2>Scan QR Code</h2>
+                <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Phone Number */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Personal Details</h2>
-              <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+            {/* Phone Number */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Personal Details</h2>
+                <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+              </div>
+              <img src={personal} />
             </div>
-            <img src={personal} />
-          </div>
 
-          {/* Facial Recognition */}
-          <div className={styles.flexRowSS}>
-            <img src={facial} />
-            <div>
-              <h2>Facial Recognition</h2>
-              <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+            {/* Facial Recognition */}
+            <div className={styles.flexRowSS}>
+              <img src={facial} />
+              <div>
+                <h2>Facial Recognition</h2>
+                <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Sign In */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Sign In</h2>
-              <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+            {/* Sign In */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Sign In</h2>
+                <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+              </div>
+              <img src={signin} />
             </div>
-            <img src={signin} />
-          </div>
 
-        </main>
+            <section className={styles.platforms}>
+              {/* IOS */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={appleIcon} alt="iOS" className={styles.platformIcon} />
+                  <div className={styles.flexbox}>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+
+              {/* Android */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={androidIcon} alt="Android" className={styles.platformIcon} />
+                  <div>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+            </section>
+
+          </main>
         );
       case "signout":
         return (
           <main className={styles.main}>
-          <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
+            <h1 style={{ fontSize: '2.2rem' }}>Contractor Sync</h1>
 
-          {/* Sign In process */}
-          <div className={styles.flexRow}>
-            <div>
-              <h2>Sign Out Process</h2>
-              <p>4 step process, it will take 2 mins of time</p>
-              <a href="#">https://fiacon.com/signin</a>
+            {/* Sign In process */}
+            <div className={styles.flexRow}>
+              <div>
+                <h2>Sign Out Process</h2>
+                <p>4 step process, it will take 2 mins of time</p>
+                <a href="#">https://fiacon.com/signin</a>
+              </div>
+              <img src={cleanerIcon} />
             </div>
-            <img src={cleanerIcon} />
-          </div>
 
-          {/* Scan QR Code */}
-          <div className={styles.flexRowSS}>
-            <img src={qr} />
-            <div>
-              <h2>Scan QR Code</h2>
-              <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+            {/* Scan QR Code */}
+            <div className={styles.flexRowSS}>
+              <img src={qr} />
+              <div>
+                <h2>Scan QR Code</h2>
+                <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Phone Number */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Personal Details</h2>
-              <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+            {/* Phone Number */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Personal Details</h2>
+                <p>Enter your Australian mobile number, which must be 9 digits long and start with ‘0’; only valid Australian formats are accepted. The system will automatically validate the number, and if it is already registered or not approved by Fiacon, an error message will be displayed.</p>
+              </div>
+              <img src={personal} />
             </div>
-            <img src={personal} />
-          </div>
 
-          {/* Facial Recognition */}
-          <div className={styles.flexRowSS}>
-            <img src={facial} />
-            <div>
-              <h2>Facial Recognition</h2>
-              <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+            {/* Facial Recognition */}
+            <div className={styles.flexRowSS}>
+              <img src={facial} />
+              <div>
+                <h2>Facial Recognition</h2>
+                <p>Use your device’s front-facing camera to capture a clear image of your face, which will be securely registered for future biometric sign-in and sign-out. If the captured image is unclear, too dark, or obstructed, the system will prompt you to retry.</p>
+              </div>
             </div>
-          </div>
 
-          {/* Sign In */}
-          <div className={styles.flexRowSS}>
-            <div>
-              <h2>Sign In</h2>
-              <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+            {/* Sign In */}
+            <div className={styles.flexRowSS}>
+              <div>
+                <h2>Sign In</h2>
+                <p>Answer the five required questions related to cleaning safety, compliance, and readiness; you must complete all answers to proceed. These responses help validate your eligibility and preparedness for work responsibilities</p>
+              </div>
+              <img src={signin} />
             </div>
-            <img src={signin} />
-          </div>
 
-        </main>
+            <section className={styles.platforms}>
+              {/* IOS */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={appleIcon} alt="iOS" className={styles.platformIcon} />
+                  <div className={styles.flexbox}>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+
+              {/* Android */}
+              <div className={styles.platformCardBox}>
+                <div className={styles.platformCard}>
+                  <img src={androidIcon} alt="Android" className={styles.platformIcon} />
+                  <div>
+                    <div className={styles.platformTitle}>Contractor sync</div>
+                    <div className={styles.cmsTitle}>Sign In and Sign Up</div>
+                  </div>
+                  <button className={styles.installButton}>Install</button>
+                </div>
+              </div>
+            </section>
+
+          </main>
         );
       default:
         return <h1>Invalid Tab</h1>;
